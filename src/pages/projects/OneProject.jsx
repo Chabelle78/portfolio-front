51088b1be5e9f3cdd767b1de2e152347649508b6
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-07-27 13:45:43 +0200

    fetch pictures of the project

--- src/pages/projects/OneProject.jsx
+++ src/pages/projects/OneProject.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useHistory, useParams } from "react-router";
 import { project } from "../../API/request";
+import PicturesProjects from "./PicturesProjects";
 
 export default function OneProject() {
   const { id } = useParams();
@@ -15,6 +16,7 @@ export default function OneProject() {
     <div>
       <h1 className="text-3xl mb-12">{data?.name}</h1>
       <p className=" mx-7 pr-20 my-6">{data?.description}</p>
+      <PicturesProjects {...data} key={data.id} />
       <button
         onClick={() => history.goBack()}
         className="focus:outline-none w-28 h-8 sm:text-sm text-xs text-white bg-customGreen px-2 py-1 mt-5 sm:mt-0 sm:p-2 shadow-buttonShadow rounded-md flex justify-center items-center"
